4271a22fcb07067cc76f3c47c6e02e088a0581ae
sojwalwherrelz <104896718+sojwalwherrelz@users.noreply.github.com>
2022-05-06 14:25:20 +0530

    Add files via upload
    Main file Added For calling all Api code

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react"
+import Countries from "./components/Countries"
+// import CurrentMatch from "./components/CurrentMatch"
+// import MatchList from "./components/MatchList"
+// import Players from "./components/Players"
+// import PlayerSearch from "./components/PlayerSearch"
+// import Seriesinfo from "./components/Seriesinfo"
+// import FantacySquad from "./components/FantacySquad"
+// import MatchScorecard from "./components/MatchScorecard"
+// import MatchPoints from "./components/MatchPoints"
+
+
+class App extends Component{
+
+  render()
+  {
+    return (
+
+        <div className="center"> 
+        
+        <Countries/>
+        {/* <CurrentMatch/> */}
+        {/* <MatchList/> */}
+        {/* <Players/> */}
+        {/* <PlayerSearch/> */}
+        {/* <Seriesinfo/> */}
+        {/* <FantacySquad/> */}
+        {/* <MatchScorecard/> */}
+        {/* <MatchPoints/> */}
+        
+         </div>
+
+    )
+  } 
+}
+
+export default App
+
